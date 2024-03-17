60aef814c58bd6ef2b0676760f1327df1177743b
Aigul Ysakova <ysakova90@gmail.com>
2024-03-17 11:59:12 -0500

    ch

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,35 +1,11 @@
 output "vpc_id" {
   value = module.vpc-team1.vpc_id
 }
-output "public_subnet1" {
-  value = module.vpc-team1.public_subnet1
+output private_subnets {
+  description = "Prints out private_subnets"
+  value = module.vpc.private_subnets
 }
-output "public_subnet2" {
-  value = module.vpc-team1.public_subnet2
+output public_subnet {
+  description = " Prints out public_subnets"
+  value = module.vpc. public_subnets
 }
-output "public_subnet3" {
-  value = module.vpc-team1.public_subnet3
-}
-output "private_subnet1" {
-  value = module.vpc-team1.private_subnet1
-}
-output "private_subnet2" {
-  value = module.vpc-team1.private_subnet2
-}
-output "private_subnet3" {
-  value = module.vpc-team1.private_subnet3
-}
-output "az1" {
-  value = module.vpc-team1.az1
-}
-output "az2" {
-  value = module.vpc-team1.az2
-}
-output "az3" {
-  value = module.vpc-team1.az3
-}
-
-
-# output all {
-#     value = terraform_remote_state.remote.outputs.*
-# }
