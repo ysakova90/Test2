653beeeac07fe72e6c234d0d08c01bfe53bcf12a
Aigul Ysakova <ysakova90@gmail.com>
2024-03-16 23:54:01 -0500

    added

---
+++ vpc/output.tf
@@ -0,0 +1,8 @@
+output private_subnets {
+  description = "Prints out private_subnets"
+  value = module.vpc.private_subnets
+}
+output public_subnet {
+  description = " Prints out public_subnets"
+  value = module.vpc. public_subnets
+}
